7c028eaf2b072bb409842213dad2f5ffb8982724
davitgabo <datigabashvili@gmail.com>
2022-03-07 22:04:56 +0400

    Add more pages to the form

--- questionnaireFormPage/questionnaireFormPage.js
+++ questionnaireFormPage/questionnaireFormPage.js
@@ -1,3 +1,11 @@
+/* variables */
+let skillsArr = [];
+
+const token = "6cd43df6-7d5b-4ae7-9aa3-422a6768162b";
+const Http = new XMLHttpRequest();
+const skillsGetUrl = "https://bootcamp-2022.devtest.ge/api/skills";
+const questionnarePostUrl = "https://bootcamp-2022.devtest.ge/api/application"
+
 /* functions */
 
 function hideAll() {
@@ -67,7 +75,7 @@ function appendClass(respond, id) {
 }
 
 function addProgrammingLanguage() {
-  let skillId = document.getElementById("skills").value;
+  let skillId = document.getElementById("skills-select").value;
   let skillName = document.getElementById("skillId-" + skillId).innerHTML;
   let skillDuration = document.getElementById("duration").value;
   let skillDivId = document.getElementById("skill-" + skillId);
@@ -86,35 +94,100 @@ function addProgrammingLanguage() {
                   <div><img src="../assets/img/Remove.png" id="removeBtn-${skillId}" onclick="removeSkill(${skillId})" alt="remove" /></div>
                 </div>
     `;
+    skillsArr.push({
+      id: skillId,
+      experience: skillDuration
+    })
   }
+
+  
+
 }
 
 function removeSkill(skillId) {
   document.getElementById("skill-" + skillId).remove();
+  skillsArr = skillsArr.filter(skill => skill.id != skillId);
 }
 
 function appendSkills(skills) {
   skills.forEach((skill) => {
-    document.getElementById("skills").innerHTML += `
+    document.getElementById("skills-select").innerHTML += `
   
   <option value="${skill.id}" id="skillId-${skill.id}">${skill.title}</option>
   `;
   });
 }
 
+function generateBodyForSendRequest() {
+  const first_name = document.getElementById("firstName").value;
+  const last_name = document.getElementById("lastName").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("number").value;
+  const skills = skillsArr
+  const work_preference = document.querySelector("input[name='work_preference']:checked")?.value;
+  const had_covid = document.querySelector("input[name='covid_contraction']:checked")?.value
+  const had_covid_at = document.getElementById("had_covid_at").value;
+  const vaccinated = document.querySelector("input[name='vaccinated']:checked")?.value
+  const vaccinated_at = document.getElementById("had_vaccine_at").value
+  const will_organize_devtalk = document.querySelector("input[name='attendance']:checked")?.value
+  const devtalk_topic = document.getElementById("devtalk_subject").value
+  const something_special = document.getElementById("something_special").value
+
+  return  reqBody = {
+    token: token,
+    first_name: first_name,
+    last_name: last_name,
+    email: email,
+    phone: phone,
+    skills: skills,
+    work_preference: work_preference,
+    had_covid: had_covid == "true",
+    had_covid_at: had_covid_at,
+    vaccinated: vaccinated == "true",
+    vaccinated_at: vaccinated_at,
+    will_organize_devtalk: will_organize_devtalk == "true",
+    devtalk_topic: devtalk_topic,
+    something_special: something_special,
+  }
+}
+
+function submitForm() {
+  sendQuestionnareFormWithHttpReq();
+}
+
 /* Services */
 
 function getSkills() {
-  const Http = new XMLHttpRequest();
-  const url = "https://bootcamp-2022.devtest.ge/api/skills";
-  Http.open("GET", url);
-  Http.send();
-  Http.onreadystatechange = (e) => {
-    appendSkills(JSON.parse(Http.responseText));
-  };
+  fetch(skillsGetUrl, {
+    method: "GET",
+    headers: {'Content-Type': 'application/json', 'accept': 'application/json'}, 
+  }).then(res => res.json()).then(skills => {
+    appendSkills(skills);
+  })
 }
-
 getSkills();
+
+function sendQuestionnareFormWithHttpReq() {
+  let reqBody = generateBodyForSendRequest()
+
+  fetch(questionnarePostUrl, {
+    method: "POST",
+    headers: {'Content-Type': 'application/json', 'accept': 'application/json'}, 
+    body: JSON.stringify(reqBody)
+  }).then(res => {
+    console.log(res)
+    if(res.ok) {
+      document.getElementById("submit-button").className = "qp-hidden";
+      document.getElementById("thank-you-text").className = ""
+
+      setTimeout(function() {
+        window.location.href = '../submittedApplicationsPage/submittedApplicationsPage.html'
+      }, 3000)
+      
+    }
+  })
+
+}
 
 /* DOM */
 
